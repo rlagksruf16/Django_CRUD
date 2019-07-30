d2d1605d6cfd0dc3c400b7e7144b1c461803296f
rlagksruf16 <rlagksruf16@gmail.com>
2019-07-31 00:41:52 +0900

    edit collectstatic

--- second_project/settings.py
+++ second_project/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
